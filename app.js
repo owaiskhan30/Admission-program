a53e047eb3f1a75fe682b9c635fd52065d92f439
owaiskhan30 <owaiskhan.raees@gmail.com>
2024-08-29 01:18:58 +0500

    Changes

--- app.js
+++ app.js
@@ -2,11 +2,7 @@ function admission(name, age) {
   if (age >= 18) {
     console.log("Welcome to the Gym" + " " + name);
   } else if (age >= 15) {
-    console.log(
-      name +
-        " " +
-        "ap ka admission ho jaye ga magar ap ko extra pay krna ho ga....."
-    );
+        console.log(name + " " + "ap ka admission ho jaye ga magar ap ko extra pay krna ho ga.....");
   } else {
     console.log(name + " " + "Ap ki Age kam hai...");
   }
